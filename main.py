96c69cf933c87feb70d9e12f78556f3098230532
alplex4-sys <311law93@gmail.com>
2026-03-26 19:54:44 +0200

    Сканирование адреса или пула адресов на открыте порты

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import socket
 import subprocess
+import re
 
-# Функция проверки "живой" ли узел
+
 def check_ping(ip):
-    
-    response = subprocess.run(['ping', '-n', '1', '-w', '500', ip], stdout=subprocess.PIPE)
-    return response.returncode == 0
 
-# Функция проверки конкретного порта
+    process = subprocess.run(['ping', '-n', '1', '-w', '500', ip], capture_output=True, text=True)
+    return "TTL=" in process.stdout
+
+
 def check_port(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(0.5)
@@ -15,18 +16,40 @@ def check_port(ip, port):
     sock.close()
     return result == 0
 
-# Основная логика
-targets = ["8.8.8.8", "127.0.0.1","192.168.0.1","62.141.122.126"] 
-common_ports = [21, 22, 80, 443, 3389]
 
-print("=== Scanning Network ===")
-for ip in targets:
-    if check_ping(ip):
-        print(f"\n[+] {ip} is ONLINE")
-        for port in common_ports:
-            if check_port(ip, port):
-                print(f"    Port {port}: OPEN")
-    else:
-        print(f"[-] {ip} is OFFLINE")
+while True:
+    print("\n" + "="*30)
+    print("=== Network Scanner ===")
+    print("Введите 'exit' для выхода")
+    user_input = input("Введите IP-адреса через запятую: ").strip().lower()
+
+ 
+    if user_input == 'exit':
+        print("Завершение работы...")
+        break
+
+
+    targets = [ip.strip() for ip in user_input.split(",") if ip.strip()]
+
+    if not targets:
+        print("[!] Ошибка: Вы ничего не ввели.")
+        continue
+
+    print("\n--- Начало сканирования ---")
+    for ip in targets:
        
-input()
+        if not re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip):
+            print(f"[!] {ip} - Некорректный формат IP-адреса")
+            continue
+
+        if check_ping(ip):
+            print(f"\n[+] {ip} is ONLINE")
+            common_ports = [21, 22, 80, 443, 3389]
+            for port in common_ports:
+                if check_port(ip, port):
+                    print(f"    Port {port}: OPEN")
+        else:
+            print(f"[-] {ip} is OFFLINE")
+
+
+input("\nPress enter to pay respect!")
